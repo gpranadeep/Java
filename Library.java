84c32c0b3f9d1dcc850fb04b5b41275dab5124f5
G Pranadeep <gampalapranadeep.25.csm@anits.edu.in>
2026-08-07 08:13:02 +0530

    Update Library.java
    Added new classes for abstraction and interfaces, including Shape, Circle, Rectangle, and Printable. Updated Library class to demonstrate new features.

--- Library.java
+++ Library.java
@@ -3,43 +3,53 @@ class Book {
     String bookName;
     String author;
     double price;
+
     Book() {
         bookId = 1;
         bookName = "Java Programs";
         author = "Oracle";
         price = 189.99;
     }
+
     Book(int bookId, String bookName, String author, double price) {
         this.bookId = bookId;
         this.bookName = bookName;
         this.author = author;
         this.price = price;
     }
+
     public int getBookId() {
         return bookId;
     }
+
     public String getBookName() {
         return bookName;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public double getPrice() {
         return price;
     }
-    
+
     public void setBookId(int bookId) {
         this.bookId = bookId;
     }
+
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     public void displayBook() {
         System.out.println("Book ID   : " + bookId);
         System.out.println("Book Name : " + bookName);
@@ -51,15 +61,17 @@ class Book {
 class Person {
     String name;
     int age;
-    Person(String name, int age) {
+
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 }
+
 class Student extends Person {
     int roll;
 
-    Student(String name, int age, int roll) {
+    public Student(String name, int age, int roll) {
         super(name, age);
         this.roll = roll;
     }
@@ -70,22 +82,27 @@ class Student extends Person {
         System.out.println("Roll No      : " + roll);
     }
 }
+
 class Faculty extends Person {
     String sub;
+
     Faculty(String name, int age, String sub) {
         super(name, age);
         this.sub = sub;
     }
+
     void display() {
         System.out.println("Faculty Name : " + name);
         System.out.println("Age          : " + age);
         System.out.println("Subject      : " + sub);
     }
 }
+
 class Area {
     double area(double radius) {
         return 3.14 * radius * radius;
     }
+
     int area(int length, int breadth) {
         return length * breadth;
     }
@@ -94,28 +111,61 @@ class Area {
         return 0.5 * base * height;
     }
 }
+
 class Vehicle {
     void display() {
         System.out.println("This is a Vehicle.");
     }
 }
+
 class Car extends Vehicle {
     @Override
     void display() {
         System.out.println("This is a Car.");
     }
 }
+
 class Bike extends Vehicle {
     @Override
     void display() {
         System.out.println("This is a Bike.");
     }
 }
-public class Library{
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+interface Printable {
+    void print();
+}
+
+class Report implements Printable {
+    @Override
+    public void print() {
+        System.out.println("Printing Library Report");
+    }
+}
+
+public class Library {
     public static void main(String[] args) {
         System.out.println("Book Class");
-        Book b1 = new Book(); 
-        Book b2 = new Book(101,"Java Programming","James Gosling",650.50);
+        Book b1 = new Book();
+        Book b2 = new Book(101, "Java Programming", "James Gosling", 650.50);
         b1.displayBook();
         System.out.println();
         b2.displayBook();
@@ -138,5 +188,13 @@ public class Library{
         v.display();
         Bike b = new Bike();
         b.display();
+        System.out.println("\nAbstraction");
+        Shape c = new Circle();
+        Shape r = new Rectangle();
+        c.draw();
+        r.draw();
+        System.out.println("\nInterface");
+        Printable p = new Report();
+        p.print();
     }
 }
