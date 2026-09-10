64113e1043f2b4b0a915ebe3f729dbe217b57b4c
G Pranadeep <gampalapranadeep.25.csm@anits.edu.in>
2026-09-10 22:58:19 +0530

    Add ClassNotFound.java to handle class loading errors

---
+++ assignments/Assignment-3/ClassNotFound.java
@@ -0,0 +1,9 @@
+public class ClassNotFound{
+    public static void main(String[] args) {
+        try {
+            Class.forName("UnknownClass");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found.");
+        }
+    }
+}
