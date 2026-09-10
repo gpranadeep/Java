6d5a564adb7a377724e80ba23aebc08b2398ecda
G Pranadeep <gampalapranadeep.25.csm@anits.edu.in>
2026-09-10 22:36:24 +0530

    Create Concat.java

---
+++ assignments/String_Operations/Concat.java
@@ -0,0 +1,7 @@
+class Concat{
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = " World";
+        System.out.println(first + second);
+    }
+}
